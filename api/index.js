d65b310010642fb4583d1fcd2df4b205895070c7
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-07-17 21:41:11 +0530

    please

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173/",
+    origin: "https://makeourtrip-rho.vercel.app",
   })
 );
 
